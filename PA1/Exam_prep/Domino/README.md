971df043414ebd97ca8a9d8109d7d33e87323338
Volodymyr Plita <plitavol@fit.cvut.cz>
2023-01-21 16:59:52 +0100

    domino: Add areSame.c

---
+++ PA1/Exam_prep/Domino/README.md
@@ -0,0 +1,26 @@
+# Dominos
+EN
+We have triangle, square or pentagon shaped dominoes. The dice contain numbers that can be positive, negative or zero and may be repeated on the dice.
+
+The program receives dice written as threes/fours/fives of numbers up to EOF. Subsequently, he has the task of listing the number of different dice.
+
+Dice are the same if they contain the same numbers in the same order, but they may be turned arbitrarily. Thus:
+
+[4,8,1,3] = [4,8,1,3]
+[1,2,3] = [2,3,1] → rotated by 120°
+[2,5,7,2,5] = [7,2,5,2,5] → rotated twice by 72°
+[1,2,3] != [1,3,2]
+
+---
+
+CZ
+Máme kostky domina ve tvaru trojúhelníku, čtverce nebo pětiúhelníku. Kostky obsahují čísla, která můžou být kladná, záporná i nulová a na kostce se smí opakovat.
+
+Program dostává kostky zapsané jako trojici/čtveřici/pětici čísel až do EOF. Následně má za úkol vypsat počet různých kostek.
+
+Kostky jsou stejné, pokud obsahují stejná čísla ve stejném pořadí, smí být ale libovolně natočené. Tedy:
+
+[4,8,1,3] = [4,8,1,3]
+[1,2,3] = [2,3,1] → otočeno o 120°
+[2,5,7,2,5] = [7,2,5,2,5] → otočeno dvakrát o 72°
+[1,2,3] != [1,3,2]
---
+++ PA1/Exam_prep/Domino/areSame.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int areSame(int * c1, int * c2);
+
+int main()
+{
+    int d1[5] = {2, 5, 7, 2, 5};
+    int d2[5] = {2, 5, 2, 5, 7};
+    int d3[5] = {7, 2, 5, 2, 5};
+    int d4[5] = {4, 6, 7, 8, 9};
+    int d5[5] = {2, 7, 5, 2, 5};
+
+    areSame(d1, d2) == 1 ? printf("1 | 2 - same\n") : printf("1 | 2 - different\n");
+    areSame(d1, d3) == 1 ? printf("1 | 3 - same\n") : printf("1 | 3 - different\n");
+    areSame(d1, d4) == 1 ? printf("1 | 4 - same\n") : printf("1 | 4 - different\n");
+    areSame(d1, d5) == 1 ? printf("1 | 5 - same\n") : printf("1 | 5 - different\n");
+
+    areSame(d2, d3) == 1 ? printf("2 | 3 - same\n") : printf("2 | 3 - different\n");
+    areSame(d2, d4) == 1 ? printf("2 | 4 - same\n") : printf("2 | 4- different\n");
+    areSame(d2, d5) == 1 ? printf("2 | 5 - same\n") : printf("2 | 5 - different\n");
+
+    areSame(d3, d4) == 1 ? printf("3 | 4 - same\n") : printf("3 | 4 - different\n");
+    areSame(d3, d5) == 1 ? printf("3 | 5 - same\n") : printf("3 | 5 - different\n");
+
+    areSame(d4, d5) == 1 ? printf("4 | 5 - same\n") : printf("4 | 5 - different\n");
+
+    return 0;
+}
+
+int areSame(int * c1, int * c2)
+{
+    int n = 5;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (c1[i] == c2[j])
+            {
+                int old_i = i;
+                int old_j = j;
+                int found = 1;
+                for (int k = 0, p = n - 1; k < p; k++)
+                {
+                    i = (i + 1) % 5;
+                    j = (j + 1) % 5;
+                    if (c1[i] != c2[j])
+                    {
+                        found = 0;
+                        break;
+                    }
+                }
+                i = old_i;
+                j = old_j;
+                    
+                if (found == 1) return 1;
+            }
+        }
+    }
+    return 0;
+}
